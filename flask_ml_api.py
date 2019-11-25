3f878d08e95baac549ea7bf9d8add740ee486c05
Awantik Das <awantikdas@gmail.com>
2019-11-25 17:13:55 +0530

    Add files via upload

--- flask_ml_api.py
+++ flask_ml_api.py
@@ -8,12 +8,9 @@ app = Flask(__name__)
 @app.route('/predict/tx', methods=['GET'])
 def create_task():
     tx_data = request.json
-    #model = joblib.load('model0.pipeline')
-    model.predict([[]])
-    #tx_data['P'] = model.best_estimator_.predict(df)[0]
-    #cwd.write_json_table(tx_data)
-    #tx_data['P'] = str(tx_data['P'])
-    return jsonify("Hello World"), 201
+    model = joblib.load('house.model')
+    res = model.predict([[1177.698,2,7,2,2,2]])
+    return jsonify(res[0]), 201
 
 if __name__ == '__main__':
     app.run(debug=True)
